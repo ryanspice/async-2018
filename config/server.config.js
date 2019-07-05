ce59501eb36159f9fbd38f11de2636be0b815f04
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2019-07-04 23:28:34 -0400

    removing proxies from a copy

--- config/server.config.js
+++ config/server.config.js
@@ -1,40 +1,6 @@
 module.exports = {
    "proxy": {
-      "/api/dealerblock/**": {
-         "target": "https://0u45cbfika.execute-api.us-east-1.amazonaws.com/testdev/",
-         "secure": false,
-         "changeOrigin": true
-      },
-      "/api/**": {
-         "target": "https://us-east-1-testdev.nw.adesa.com/",
-         "secure": false,
-         "changeOrigin": true
-      },
-      "/search/**": {
-         "target": "https://nw-search-apigw-testdev.nw.adesa.com/testdev/api/",
-         "secure": false,
-         "changeOrigin": true
-      },
-      "/assets/img/**": {
-        "target": "https://us-east-1-testdev.nw.adesa.com/",
-        "secure": false,
-        "changeOrigin": true
-      },  
-      "/assets/js/**": {
-        "target": "https://us-east-1-testdev.nw.adesa.com/",
-        "secure": false,
-        "changeOrigin": true
-      },           
-      "/static-components/**": {
-         "target": "https://us-east-1-testdev.nw.adesa.com/",
-         "secure": false,
-         "changeOrigin": true
-      },
-      "/apps/**": {
-         "target": "https://us-east-1-testdev.nw.adesa.com/",
-         "secure": false,
-         "changeOrigin": true
-      }
+
    },
 
     "historyApiFallback": true,
