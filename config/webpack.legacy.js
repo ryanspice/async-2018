292854a89aa2ec9ea590dc68b93daeda356d263f
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2019-08-06 00:07:53 -0400

    legacy name fix

--- config/webpack.legacy.js
+++ config/webpack.legacy.js
@@ -16,7 +16,7 @@ module.exports = legacy = evt => {
 	temp.output.library = `${package.short_name}_legacy`;
 	temp.output.chunkFilename = `[name].legacy.js`;
 	temp.output.filename = `[name].legacy.js`;
-	temp.entry[name] = `./src`;
+	temp.entry[package.short_name] = `./src`;
 /*
 	entry[`${package.short_name}`]=`webpack-polyfill-injector?${JSON.stringify({
 			modules: './src' // list your entry modules for the `app` entry chunk
